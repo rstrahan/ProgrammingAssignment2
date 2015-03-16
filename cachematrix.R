2ee8ff09acf9e8b69122b6909e1cbc351f3dd2eb
Bob Strahan <bob.strahan@gmail.com>
2015-03-16 17:34:17 -0400

    Functions completed and tested

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 
 ## makeCacheMatrix: creates a special "matrix" object that can cache its inverse
+## Usage:
+## Initialize matrix object, eg: cache=makeCacheMatrix()
+## 'set' method to assign a new matrix into the cache, eg: cache$set(myMatrix)
+## 'get' method to retrieve cached matrix, eg cache$get()
+## 'setInverse' and 'getInverse' methods are used by cacheSolve() function below.
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
@@ -21,6 +26,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve: computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## Usage:
+##  invMatrix=cacheSolve(cache)
+
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -38,26 +46,53 @@ cacheSolve <- function(x, ...) {
 ####################
 ## Tests / Examples 
 ####################
+
+## Initialize matrix cache object
 # cache = makeCacheMatrix()
 #
-# myMatrix=matrix(c(4,2,6,8),2,2) # arbitrary 2x2 matrix
+## create an arbitrary 2x2 matrix for testing
+# myMatrix=matrix(c(4,2,6,8),2,2) 
+
+## Set the value into the matrix cache, using the 'set' method
 # cache$set(myMatrix)
+
+## Find the inverse of our test matrix using the cacheSolve function
 # myMatrix_Inv = cacheSolve(cache)
-# myMatrix_Inv %*% myMatrix # multiply original by its inverse - always get matrix I below
+
+## test if we got a true inverse by multiplying the
+## original by its inverse - should always get the IDENTITY matrix below
+#
+# myMatrix_Inv %*% myMatrix
 #
 #       [,1] [,2]
 # [1,]    1    0
 # [2,]    0    1
 
-## make sure it gets cached data when called a second time (check that it prints the message)
+## can get both the original and inverse matrices from the cache object
+# o <- cache$get()
+# i <- cache$getinverse()
+# o %*% i
+#
+#       [,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
+
+
+## Make sure cacheSolve() retrieves the cached data when called a second time 
+## (check that it prints the message "getting cached data")
 # myMatrix_Inv = cacheSolve(cache)
 # getting cached data
 
-## change the matrix, make sure it recomputes and recaches the new inverse
+## Change the matrix data, and verify the cacheSolve() recomputes and 
+## recaches the new inverse
 # newMatrix=matrix(c(3,4,2,5),2,2)
 # cache$set(newMatrix)
 # newMatrix_Inv = cacheSolve(cache)
-
+# newMatrix_Inv %*% newMatrix
+#
+#       [,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
 
 
 
